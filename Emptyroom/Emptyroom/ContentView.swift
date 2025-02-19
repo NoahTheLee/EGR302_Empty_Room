ebfa6cf74d2c54ac622ce19b490e30fd8ee0d14b
Luca Imbriolo <lucaimbriolo@gmail.com>
2025-02-19 11:12:27 -0800

    All my work for sprint 2

---
+++ Emptyroom/Emptyroom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Emptyroom
+//
+//  Created by Luca Vincent Imbriolo on 2/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
