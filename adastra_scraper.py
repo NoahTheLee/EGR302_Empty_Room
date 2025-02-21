e782664fd0e42a353550e8768259ee9a122951e5
Ethan Walden <EthanScott.Walden@calbaptist.edu>
2025-02-21 14:16:02 -0800

    progress on scraper; successfully logged in to ad astra

--- adastra_scraper.py
+++ adastra_scraper.py
@@ -1,18 +1,68 @@
-# WIP!!! THIS CURRENTLY EXISTS ON GITHUB JUST FOR MY INDIVIDUAL PROGRESS DEMONSTRATION.
+# Hi there! I'm making good progress on this. I have successfully made selenium log in to ad astra.
 
-# THIS WAS MY FIRST ATTEMPT AT THE WEB SCRAPER FOR AD ASTRA THAT I MADE DURING MY RESEARCH. YES, IT'S REALLY THAT SMALL.
-# IF YOU WANT TO TRY IT FOR YOURSELF, REPLACE THE FILE PATH WITH YOUR OWN VERSION OF ADASTRA'S HTML; NO WAY I'M UPLOADING THAT TO GITHUB.
-# ALTHOUGH THE LOGIC IS SOLID, THIS SCRAPER DOES NOT WORK YET BECAUSE HTML OBTAINED DIRECTLY FROM THE SITE CANNOT CONTAIN ALL OF THE CLASSROOM DATA; 
-# ADASTRA LOADS IN THE DATA WITH JAVASCRIPT DYNAMICALLY DEPENDING ON THE POSITION OF THE SCROLL BAR.
+# This script has required arguments. To run this, type this in the console:
+# python adastra_scraper.py <username> <password>
+# where <username> and <password> are the credientials you want to try to use to log in to ad astra.
 
-# ALSO I'LL MAKE THE VARIABLE NAMES MORE PROFESSIONAL AND ADD MORE DOCUMENTATION IN THE NEXT VERISONS. :)
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+import argparse
+from time import sleep
 
-from bs4 import BeautifulSoup
+# Setting up argument parser for username and password, which will be used to attempt to login to ad astra
+parser = argparse.ArgumentParser()
+parser.add_argument('username', help='Username for Ad Astra')
+parser.add_argument('password', help='Password for Ad Astra')
+args = parser.parse_args()
 
-soup = BeautifulSoup(open("C:\\Users\\Ethan\\Downloads\\pain.html"), "html.parser")
+username = args.username
+password = args.password
 
-the_list = soup.select("#tableview-1040 tbody tr .x-grid-cell-first div")
+# The webdriver i'm using is microsoft edge... you can change this to chrome or firefox if you want but i'll have to go download the driver for that
+driver = webdriver.Edge()
+driver.get("https://www.aaiscloud.com/CABaptistU/logon.aspx?nosso=")
 
-for item in the_list:
-    the_data = item.text.strip()
-    print(the_data)
+# username field
+username = driver.find_element(By.ID, 'userNameField-inputEl')
+username.send_keys(args.username)
+
+sleep(0.02)
+
+# password field
+password = driver.find_element(By.ID, 'textfield-1030-inputEl')
+password.send_keys(args.password)
+
+sleep(0.02)
+
+# login button
+login = driver.find_element(By.ID, 'logonButton')
+
+try:
+    login.click()
+
+    # wait until the home page is loaded
+    WebDriverWait(driver, 5).until(lambda driver: driver.current_url == "https://www.aaiscloud.com/CABaptistU/Default.aspx")
+except:
+    # failure to login
+    print("Login failed. Either you gave an incorrect username and password as your arguments, or adastra took to long to display the homepage. \nPlease check your username and password.")
+    driver.quit()
+    exit()
+
+# all this stuff below was just test code I wrote when using selenium to navigate httpbin.org for my own understanding
+
+# form_link = driver.find_element(By.CSS_SELECTOR, 'a[href="/forms/post"]')
+# form_link.click()
+
+# custname = driver.find_element(By.CSS_SELECTOR, '[name="custname"]')
+# custname.send_keys("John Doe")
+
+# custemail = driver.find_element(By.CSS_SELECTOR, '[name="custemail"]')
+# custemail.send_keys("bungus@bungus.com")
+
+# submit = driver.find_element(By.CSS_SELECTOR, 'button')
+# sleep(1)
+# submit.click()
+
+# sleep(5)
+# driver.quit()
