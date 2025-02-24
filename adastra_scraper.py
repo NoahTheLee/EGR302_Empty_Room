a5f143cf8b3e4bb6a619217a26f4f920100f88c6
Ethan Walden <EthanScott.Walden@calbaptist.edu>
2025-02-24 12:46:43 -0800

    scraper now successfully switches to Full_ Grid calendar

--- adastra_scraper.py
+++ adastra_scraper.py
@@ -6,48 +6,92 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
 import argparse
 from time import sleep
 
-# Setting up argument parser for username and password, which will be used to attempt to login to ad astra
-parser = argparse.ArgumentParser()
-parser.add_argument('username', help='Username for Ad Astra')
-parser.add_argument('password', help='Password for Ad Astra')
-args = parser.parse_args()
+# attempts to login to ad astra with the given username and password. 
+# returns boolean: True if successful, False if not
+def login(username, password, driver):
+    # navigate to the login page
+    driver.get("https://www.aaiscloud.com/CABaptistU/logon.aspx?nosso=")
 
-username = args.username
-password = args.password
+    
 
-# The webdriver i'm using is microsoft edge... you can change this to chrome or firefox if you want but i'll have to go download the driver for that
-driver = webdriver.Edge()
-driver.get("https://www.aaiscloud.com/CABaptistU/logon.aspx?nosso=")
+    # username field
+    username_field = driver.find_element(By.ID, 'userNameField-inputEl')
+    username_field.send_keys(username)
 
-# username field
-username = driver.find_element(By.ID, 'userNameField-inputEl')
-username.send_keys(args.username)
+    sleep(0.02)
 
-sleep(0.02)
+    # password field
+    password_field = driver.find_element(By.ID, 'textfield-1030-inputEl')
+    password_field.send_keys(password)
 
-# password field
-password = driver.find_element(By.ID, 'textfield-1030-inputEl')
-password.send_keys(args.password)
+    sleep(0.02)
 
-sleep(0.02)
+    # login button
+    login_button = driver.find_element(By.ID, 'logonButton')
 
-# login button
-login = driver.find_element(By.ID, 'logonButton')
+    try:
+        # click on login button
+        login_button.click()
 
-try:
-    login.click()
+        sleep(0.02)
 
-    # wait until the home page is loaded
-    WebDriverWait(driver, 5).until(lambda driver: driver.current_url == "https://www.aaiscloud.com/CABaptistU/Default.aspx")
-except:
-    # failure to login
-    print("Login failed. Either you gave an incorrect username and password as your arguments, or adastra took to long to display the homepage. \nPlease check your username and password.")
-    driver.quit()
-    exit()
+        # wait until the home page is loaded
+        WebDriverWait(driver, 5).until(lambda driver: driver.current_url == "https://www.aaiscloud.com/CABaptistU/Default.aspx")
+        return True
+    except:
+        # failure to login
+        print("Login failed. Adastra took to long to display the homepage.")
+        driver.quit()
+        return False
+
+# once we log in, we can navigate to the calendar page and scrape the classroom data.
+# returns dictionary mapped by classroom name to (WILL DECIDE DETAILS LATER)
+def get_room_data(driver):
+    # navigate to the calendar page
+    driver.get("https://www.aaiscloud.com/CABaptistU/Calendars/DailyGridCalendar.aspx")
+
+    # Once we get here, we need to switch to the correct calendar. 
+    # By default it does not tell us anything about what times each class will be occupied. We need to switch to "Full_ Grid."
+
+    # Clicks on the calendar selection dropdown arrow to show the options
+    calendar_selection_dropdown_arrow = driver.find_element(By.ID, 'DailyGridCalendarForm_CalendarComboCtrl-trigger-picker')
+    calendar_selection_dropdown_arrow.click()
+
+    sleep(0.15)
+
+    # Clicks on the "Full_ Grid" option
+    full_grid_option = driver.find_element(By.CSS_SELECTOR, '#DailyGridCalendarForm_CalendarComboCtrl-picker-listEl > li:nth-child(2)')
+    full_grid_option.click()
+
+    sleep(5)
+    return {}
+
+
+if __name__ == '__main__':
+    # Setting up argument parser for username and password, which will be used to attempt to login to ad astra
+    parser = argparse.ArgumentParser()
+    parser.add_argument('username', help='Username for Ad Astra')
+    parser.add_argument('password', help='Password for Ad Astra')
+    args = parser.parse_args()
+
+    driver = webdriver.Edge()
+
+    # attempting login. If successful, we shall commence the funny
+    if login(args.username, args.password, driver):
+        room_data = get_room_data(driver) # the funny in question
+        
+        # other stuff here eventually
+        driver.quit()
+
+        
+        
+
 
 # all this stuff below was just test code I wrote when using selenium to navigate httpbin.org for my own understanding
 
