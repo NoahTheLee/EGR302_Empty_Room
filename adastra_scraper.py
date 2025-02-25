6b1090b0fb595f82ce3fb0676f80def485cf6fa3
Ethan Walden <EthanScott.Walden@calbaptist.edu>
2025-02-24 20:04:48 -0800

    more progress not much to say about it

--- adastra_scraper.py
+++ adastra_scraper.py
@@ -18,41 +18,33 @@ def login(username, password, driver):
     # navigate to the login page
     driver.get("https://www.aaiscloud.com/CABaptistU/logon.aspx?nosso=")
 
-    
-
     # username field
     username_field = driver.find_element(By.ID, 'userNameField-inputEl')
     username_field.send_keys(username)
 
-    sleep(0.02)
+    sleep(0.01)
 
     # password field
     password_field = driver.find_element(By.ID, 'textfield-1030-inputEl')
     password_field.send_keys(password)
 
-    sleep(0.02)
+    sleep(0.01)
 
     # login button
     login_button = driver.find_element(By.ID, 'logonButton')
 
-    try:
-        # click on login button
-        login_button.click()
+    # click on login button
+    login_button.click()
 
-        sleep(0.02)
+    sleep(0.01)
 
-        # wait until the home page is loaded
-        WebDriverWait(driver, 5).until(lambda driver: driver.current_url == "https://www.aaiscloud.com/CABaptistU/Default.aspx")
-        return True
-    except:
-        # failure to login
-        print("Login failed. Adastra took to long to display the homepage.")
-        driver.quit()
-        return False
+    # wait until the home page is loaded
+    driver.get("https://www.aaiscloud.com/CABaptistU/Default.aspx")
 
-# once we log in, we can navigate to the calendar page and scrape the classroom data.
-# returns dictionary mapped by classroom name to (WILL DECIDE DETAILS LATER)
-def get_room_data(driver):
+    return driver.find_elements(By.ID, 'button-1022-btnInnerEl') != [] # returns True if we can access the calendars
+
+# Navigates driver to Full_ Grid before scraping data.
+def navigate_to_full_grid_calendar(driver):
     # navigate to the calendar page
     driver.get("https://www.aaiscloud.com/CABaptistU/Calendars/DailyGridCalendar.aspx")
 
@@ -63,14 +55,20 @@ def get_room_data(driver):
     calendar_selection_dropdown_arrow = driver.find_element(By.ID, 'DailyGridCalendarForm_CalendarComboCtrl-trigger-picker')
     calendar_selection_dropdown_arrow.click()
 
-    sleep(0.15)
+    sleep(0.25) # probably not the best way to wait for the dropdown to appear, but it works for now
 
     # Clicks on the "Full_ Grid" option
     full_grid_option = driver.find_element(By.CSS_SELECTOR, '#DailyGridCalendarForm_CalendarComboCtrl-picker-listEl > li:nth-child(2)')
     full_grid_option.click()
 
-    sleep(5)
-    return {}
+    WebDriverWait(driver, 0.5).until(lambda driver: driver.find_element(By.ID, 'DailyGridCalendarForm_CalendarComboCtrl-inputEl').get_attribute('class').find('x-form-empty-field-default') == -1)
+
+# once we log in, we can navigate to the calendar page and scrape the classroom data.
+# returns dictionary mapped by classroom name to (WILL DECIDE DETAILS LATER)
+def get_room_data(driver):
+    driver.find_element(By.CLASS_NAME, 'sch-event-inner') # this just exists to invoke implicit wait until the table is loaded
+    test = driver.find_elements(By.CSS_SELECTOR, '[id^="tableview-1040"]')
+    print(test)
 
 
 if __name__ == '__main__':
@@ -81,17 +79,18 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     driver = webdriver.Edge()
+    driver.implicitly_wait(3)
 
     # attempting login. If successful, we shall commence the funny
     if login(args.username, args.password, driver):
+        navigate_to_full_grid_calendar(driver)
         room_data = get_room_data(driver) # the funny in question
-        
+
         # other stuff here eventually
-        driver.quit()
-
-        
-        
-
+    else:
+        print("Login failed. Please check your username and password.")
+    
+    driver.quit()
 
 # all this stuff below was just test code I wrote when using selenium to navigate httpbin.org for my own understanding
 
